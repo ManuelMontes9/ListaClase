0973c87d22a2d03297f72db7cafc06e1b736132f
Many Manuel <jose_m_montes96@hotmail.com>
2017-10-17 15:31:46 -0500

    Agregar archivos de proyecto.

---
+++ ListaClase/Hijo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaClase
+{
+    class Hijo:ManejadorListas
+    {
+        List<string> listastring = new List<string>;
+
+    
+        public override void Agregar(object obj)
+        {
+            listastring.Add(obj);
+        }
+
+        public override object Consultar(int index)
+        {
+            return listastring[index];
+        }
+
+        public override void bool Eliminar(int index)
+        {
+            if (index == index)
+            {
+                listastring.Add("");
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
